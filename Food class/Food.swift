acc61b5a7af072e4ea988e2e933065bf15e55e18
Apple <apple@Apples-MacBook-Pro-46.local>
2019-08-06 15:58:10 -0400

    Im so good at classes

---
+++ Food class/Food.swift
@@ -0,0 +1,29 @@
+//
+//  Food.swift
+//  Food class
+//
+//  Created by Apple on 8/6/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+class Food {
+    var type = " "
+    var country = " "
+    var what = " "
+    
+    init(foodType : String,
+         foodCountry : String,
+         foodWhat : String)  {
+       
+        
+        type = foodType
+        country = foodCountry
+        what = foodWhat
+    }
+    func sayFood() {
+        print("\(what) is an \(type) dish from \(country)")
+    }
+}
+
